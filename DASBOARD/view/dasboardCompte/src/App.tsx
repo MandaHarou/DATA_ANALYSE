67e7e0a18a5c1708a1a5f25cbe9baffe06a15ad6
MandaHarou <Mandaharou>
2025-12-24 17:56:41 +0300

    Refactor: Implement basic dashboard layout
    Introduces the foundational structure for the dashboard, including a
    sidebar and header component. Removes unnecessary CSS and DOM setup for
    a cleaner starting point. Updates main.tsx to render the App component
    directly.

--- DASBOARD/view/dasboardCompte/src/App.tsx
+++ DASBOARD/view/dasboardCompte/src/App.tsx
@@ -1,11 +1,16 @@
 import SideBar from "./component/layot/SideBar";
-
-const App = ()=>{
-  return(
-    <div>
-      <SideBar/>
+import Header from "./component/layot/Header";
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gradiant-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
+      <div className="flex justify-start h-screen overflow-hidden">
+        <SideBar />
+      </div>
+      <div className="flex-1 flex flex-col overflow-hidden ">
+        <Header />
+      </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
--- DASBOARD/view/dasboardCompte/src/component/layot/SideBar.tsx
+++ DASBOARD/view/dasboardCompte/src/component/layot/SideBar.tsx
@@ -1,15 +1,25 @@
+import { Zap } from "lucide-react";
 const SideBar = () => {
-    return (
-        <div className="transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200 dark:border-slate-700/50 flex flex-col relative z-10 ">
-           <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50">
-              <div className="flex items-center space-x-3">
-                 <div className="w-10 h-10 bg-gradiant-to-r from-blue-600 to-purple-600  ">
-                    <h1>kk</h1>
-                 </div>
-              </div>
-            </div>
+  return (
+    <div className="transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200 dark:border-slate-700/50 flex flex-col  relative z-10 ">
+      <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50">
+        <div className="flex  items-center space-x-3">
+          <div className="w-10 h-10 bg-gradiant-to-r from-blue-600 to-purple-600 rounded-xl flex  items-center justify-center shadow-lg">
+            <Zap className="w-6 h-6 text-blue-500" />
+            {/*  Conditionala Rende   */}
+          </div>
+          <div>
+            <h1 className="text-sm font-bold text-slate-800 dark:text-blue-700">
+              DATA
+            </h1>
+            <p className="text-xs text-slate-500 dark:text-slate-400">
+              ADMIN PANEL
+            </p>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default SideBar;
