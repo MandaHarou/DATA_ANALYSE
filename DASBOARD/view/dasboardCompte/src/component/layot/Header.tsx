4e03edc41e078bd98d8c3de4602feffcf4a3f16c
MandaHarou <Mandaharou>
2025-12-28 22:20:13 +0300

    Add user profile dropdown to header
    Introduces a user profile section to the header component, including an
    avatar, name, role, and a dropdown arrow. This enhances the header's
    functionality and user information display.
    
    Also refactors the sidebar to use a menuItems array and adds navigation
    links.

--- DASBOARD/view/dasboardCompte/src/component/layot/Header.tsx
+++ DASBOARD/view/dasboardCompte/src/component/layot/Header.tsx
@@ -1,6 +1,7 @@
-import { Menu } from "lucide-react";
+import { ChevronDown, Menu } from "lucide-react";
 import { Search } from "lucide-react";
 import { Sun } from "lucide-react";
+
 const Header = () => {
   return (
     <div className="bg-white/-80 dark:bg-slate-900/80 backdrop-blur-xl border-b border-slate-200/50 dark:border-slate-700/50 px-6 py-4">
@@ -30,6 +31,22 @@ const Header = () => {
           <button className="p-2.5 rounded-xl text-slate-600 dark:text-slate-50 hover:bg-slate-100 dark:hover:slate-800 transition-colors">
             <Sun className="w-5 h-5" />
           </button>
+          <div className="flex items-center space-x-3 pl-3 border-l border-slate-200 dark:border-slate-700">
+            <img
+              src="https://cdn.iconscout.com/icon/premium/png-256-thumb/male-profile-icon-svg-download-png-4730400.png?f=webp&w=128"
+              alt="User Avatar"
+              className="w-8 h-8 rounded-full ring-2 ring-blue-500"
+            />
+            <div className="hidden md:block">
+              <p className="text-sm font-medium text-slate-500 dark:text-slate-400">
+                John Doe
+              </p>
+              <p className=" text-xs text-slate-500 dark:text-slate-400">
+                Administrator
+              </p>
+            </div>
+            <ChevronDown className="w-5 h-5 text-slate-500 dark:text-slate-400" />
+          </div>
         </div>
       </div>
     </div>
--- DASBOARD/view/dasboardCompte/src/component/layot/SideBar.tsx
+++ DASBOARD/view/dasboardCompte/src/component/layot/SideBar.tsx
@@ -1,15 +1,44 @@
-import { Zap } from "lucide-react";
+import {
+  Zap,
+  PackageSearch,
+  CircleStar,
+  FolderCog,
+  ChartCandlestick,
+} from "lucide-react";
+const menuItems = [
+  {
+    id: "Dashboard",
+    label: "Dashboard",
+    icon: ChartCandlestick,
+  },
+  {
+    id: "Product",
+    label: "Product",
+    icon: PackageSearch,
+  },
+  {
+    id: "Agence",
+    label: "Agence",
+    icon: CircleStar,
+  },
+  {
+    id: "Gestionaire",
+    label: "Gestionaire",
+    icon: FolderCog,
+  },
+];
+
 const SideBar = () => {
   return (
     <div className="transition duration-300 ease-in-out bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-r border-slate-200 dark:border-slate-700/50 flex flex-col  relative z-10 ">
       <div className="p-6 border-b border-slate-200/50 dark:border-slate-700/50">
         <div className="flex  items-center space-x-3">
           <div className="w-10 h-10 bg-gradiant-to-r from-blue-600 to-purple-600 rounded-xl flex  items-center justify-center shadow-lg">
-            <Zap className="w-6 h-6 text-blue-500" />
+            <Zap className="w-6 h-6 text-blue-500 hover:cursor-pointer" />
             {/*  Conditionala Rende   */}
           </div>
           <div>
-            <h1 className="text-sm font-bold text-slate-800 dark:text-blue-700">
+            <h1 className="text-sm font-bold text-slate-800 dark:text-blue-700 ">
               DATA
             </h1>
             <p className="text-xs text-slate-500 dark:text-slate-400">
@@ -18,6 +47,25 @@ const SideBar = () => {
           </div>
         </div>
       </div>
+      {/* Navigation */}
+      <nav className="flex-1 p-4 space-y-2 overflow-y-auto">
+        {menuItems.map((item) => (
+          <div key={item.id}>
+            <div
+              className={
+                "w-full flex items-center justify-between p-3 transition-all duration-200 border-b hover:border-blue-500 focus:outline-4 focus:outline-offset-0 focus:outline-blue-500 focus-visible:outline-4 hover:cursor-pointer"
+              }
+            >
+              <div className="flex items-center space-x-3 transition-all duration-200  ">
+                <item.icon className={"w-6 h-6 text-blue-500 "} />
+                <>
+                  <span className="font-medium ml-2 ">{item.label}</span>
+                </>
+              </div>
+            </div>
+          </div>
+        ))}
+      </nav>
     </div>
   );
 };
